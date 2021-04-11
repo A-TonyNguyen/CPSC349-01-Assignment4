83797a5c7766e9c704f6eab692f925dfc35002f5
Tony Nguyen <tonyxd14@csu.fullerton.edu>
2021-04-10 22:18:39 -0700

    Finished

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # CPSC349-01-Assignment4
+
+CPSC349 - Assignment 4
+Tony Nguyen
---
+++ scripts/firebaseconfig.js
@@ -0,0 +1,14 @@
+(function (window) {
+  'use strict';
+  window.FireBaseConfig = 
+  {
+    apiKey: "AIzaSyAU2oE9PIp7bE5jkfZzR1XkMlo2PzJy7LY",
+    authDomain: "coffeerun-6663b.firebaseapp.com",
+    databaseURL: "https://coffeerun-6663b-default-rtdb.firebaseio.com",
+    projectId: "coffeerun-6663b",
+    storageBucket: "coffeerun-6663b.appspot.com",
+    messagingSenderId: "392457035259",
+    appId: "1:392457035259:web:7fc882981d9494db7a3f5e",
+    measurementId: "G-ED0E554B2J"
+  };
+})(window);
